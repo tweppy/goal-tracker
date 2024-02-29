8ef9f09d437bc913c85e1969afc4939e9e6b5072
tweppy <tweppy@gmail.com>
2024-02-29 14:11:40 +0100

    Update getGoal function
    - Add extra auth

--- server/src/functions/goals/getGoal/index.ts
+++ server/src/functions/goals/getGoal/index.ts
@@ -3,34 +3,39 @@ import middy from "@middy/core";
 import httpErrorHandler from "@middy/http-error-handler";
 import { db } from "../../../services/db";
 import { sendResponse } from "../../../responses/index";
+import { validateTokenParam } from "../../../middleware/auth";
 
 const getGoal = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
-    const id = event.pathParameters?.goalId;
+    const goalId = event.pathParameters?.goalId;
+    const userId = event.queryStringParameters?.userId;
 
-    const result = await db
-      .get({
-        TableName: "goalsDb",
-        Key: {
-          goalId: id,
-        },
-      })
-      .promise();
+    const params = {
+      TableName: "goalsDb",
+      Key: {
+        goalId: goalId,
+      },
+    };
 
-    console.log(result.Item);
+    const result = await db.get(params).promise();
 
     if (!result.Item) {
       return sendResponse(404, { success: false, message: "Goal not found" });
     }
 
+    if (userId !== result.Item.userId) {
+      return sendResponse(401, { success: false, message: "Unauthorized user" });
+    }
+
     return sendResponse(200, {
       success: true,
       message: "Goal found",
-      body: { goal: result.Item },
+      body: { userId, goal: result.Item },
     });
   } catch (error) {
+    console.log(error);
     throw new Error("Internal Server Error");
   }
 };
 
-export const handler = middy(getGoal).use(httpErrorHandler());
+export const handler = middy(getGoal).use(httpErrorHandler()).use(validateTokenParam);
