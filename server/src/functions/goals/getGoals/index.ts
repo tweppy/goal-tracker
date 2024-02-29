bd76362563c612becc86060240dce4f8887aab38
tweppy <tweppy@gmail.com>
2024-02-29 14:10:24 +0100

    Add getGoals function

--- server/src/functions/goals/getGoals/index.ts
+++ server/src/functions/goals/getGoals/index.ts
@@ -2,18 +2,35 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { db } from "../../../services/db";
 import middy from "@middy/core";
 import { sendResponse } from "../../../responses/index";
+import httpErrorHandler from "@middy/http-error-handler";
+import { validateTokenParam } from "../../../middleware/auth";
 
 const getGoals = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
-    // code
+    const userId = event.queryStringParameters?.userId;
+
+    const params = {
+      TableName: "goalsDb",
+      FilterExpression: "userId = :userId",
+      ExpressionAttributeValues: {
+        ":userId": userId,
+      },
+    };
+
+    const userGoals = await db.scan(params).promise();
+
+    if (userGoals.Count === 0) {
+      return sendResponse(404, { success: false, message: `Failed to retrieve goals for userId: '${userId}'` });
+    }
+
     return sendResponse(200, {
       success: true,
-      message: "msg", // edit
-      // body: { },
+      message: "Goals retrieved successfully",
+      body: { userId, goals: userGoals.Items },
     });
   } catch (error) {
-    return sendResponse(500, { success: false, message: "Internal Server Error" });
+    throw new Error("Internal Server Error");
   }
 };
 
-export const handler = middy(getGoals).handler(getGoals);
+export const handler = middy(getGoals).use(httpErrorHandler()).use(validateTokenParam);
