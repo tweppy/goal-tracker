fcc90acfcefd7bbd5be33bc0a367a4aad17fb42a
tweppy <tweppy@gmail.com>
2024-03-04 11:15:42 +0100

    Add getToday function

--- server/src/functions/today/getToday/index.ts
+++ server/src/functions/today/getToday/index.ts
@@ -1,19 +1,47 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import dayjs from "dayjs";
 import { db } from "../../../services/db";
 import middy from "@middy/core";
 import { sendResponse } from "../../../responses/index";
+import httpErrorHandler from "@middy/http-error-handler";
+import { validateTokenParam } from "../../../middleware/auth";
 
 const getToday = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
-    // code
+    if (!event.queryStringParameters?.userId) {
+      return sendResponse(400, { success: false, message: "Missing valid query string parameter" });
+    }
+
+    const userId = event.queryStringParameters.userId;
+    const todayNum = dayjs().day();
+    const todayDate = dayjs().format("YYYY-MM-DD");
+
+    const params = {
+      TableName: "goalsDb",
+      FilterExpression: "userId = :userId AND (dueDate = :dueDate OR repeatType = :repeatType OR contains(repeatDay, :repeatDay))",
+      ExpressionAttributeValues: {
+        ":userId": userId,
+        ":repeatDay": todayNum,
+        ":repeatType": "daily",
+        ":dueDate": todayDate,
+      },
+    };
+
+    const todayGoals = await db.scan(params).promise();
+
+    if (todayGoals.Count === 0) {
+      return sendResponse(404, { success: false, message: "Found no goals due today" });
+    }
+
     return sendResponse(200, {
       success: true,
-      message: "msg", // edit
-      // body: { },
+      message: "Today's goals successfully retrieved",
+      body: { userId, date: todayDate, todayGoals: todayGoals.Items },
     });
   } catch (error) {
-    return sendResponse(500, { success: false, message: "Internal Server Error" });
+    console.log(error);
+    throw new Error("Internal Server Error");
   }
 };
 
-export const handler = middy(getToday).handler(getToday);
+export const handler = middy(getToday).use(httpErrorHandler()).use(validateTokenParam);
