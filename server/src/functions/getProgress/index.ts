0ffc06afe81835125862100fa739bed47cdb559e
tweppy <tweppy@gmail.com>
2024-03-05 10:22:25 +0100

    add getProgress function

--- server/src/functions/getProgress/index.ts
+++ server/src/functions/getProgress/index.ts
@@ -3,14 +3,30 @@ import { db } from "../../services/db";
 import middy from "@middy/core";
 import { sendResponse } from "../../responses/index";
 import httpErrorHandler from "@middy/http-error-handler";
+import { validateTokenParam } from "../../middleware/auth";
 
 const getProgress = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
-    // code
+    const userId = event.queryStringParameters?.userId;
+
+    const params = {
+      TableName: "completedGoalsDb01",
+      FilterExpression: "userId = :userId",
+      ExpressionAttributeValues: {
+        ":userId": userId,
+      },
+    };
+
+    const userProgress = await db.scan(params).promise();
+
+    if (userProgress.Count === 0) {
+      return sendResponse(404, { success: false, message: "No progress or completed goals found" });
+    }
+
     return sendResponse(200, {
       success: true,
-      message: "msg", // edit
-      // body: { },
+      message: "Progress retrieved successfully",
+      body: { userId, progress: userProgress.Items },
     });
   } catch (error) {
     console.log(error);
@@ -18,4 +34,4 @@ const getProgress = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxy
   }
 };
 
-export const handler = middy(getProgress).use(httpErrorHandler());
+export const handler = middy(getProgress).use(httpErrorHandler()).use(validateTokenParam);
