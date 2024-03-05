14163d990c952af518a7b61c20cda7bdbf445a35
tweppy <tweppy@gmail.com>
2024-03-05 10:49:00 +0100

    add getGoalProgress function

--- server/src/functions/getGoalProgress/index.ts
+++ server/src/functions/getGoalProgress/index.ts
@@ -3,14 +3,39 @@ import { db } from "../../services/db";
 import middy from "@middy/core";
 import { sendResponse } from "../../responses/index";
 import httpErrorHandler from "@middy/http-error-handler";
+import { validateTokenParam } from "../../middleware/auth";
 
 const getGoalProgress = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
-    // code
+    if (!event.pathParameters?.goalId || !event.queryStringParameters?.userId) {
+      return sendResponse(400, { success: false, message: "Missing valid path parameter or query string parameter" });
+    }
+
+    const goalId = event.pathParameters.goalId;
+    const userId = event.queryStringParameters.userId;
+
+    const params = {
+      TableName: "completedGoalsDb01",
+      KeyConditionExpression: "goalId = :goalId",
+      ExpressionAttributeValues: {
+        ":goalId": goalId,
+      },
+    };
+
+    const result = await db.query(params).promise();
+
+    if (!result.Items) {
+      return sendResponse(404, { success: false, message: "Goal progress not found" });
+    }
+
+    if (userId !== result.Items[0].userId) {
+      return sendResponse(401, { success: false, message: "Unauthorized user" });
+    }
+
     return sendResponse(200, {
       success: true,
-      message: "msg", // edit
-      // body: { },
+      message: "Goal found",
+      body: { userId, goal: result.Items },
     });
   } catch (error) {
     console.log(error);
@@ -18,4 +43,4 @@ const getGoalProgress = async (event: APIGatewayProxyEvent): Promise<APIGatewayP
   }
 };
 
-export const handler = middy(getGoalProgress).use(httpErrorHandler());
+export const handler = middy(getGoalProgress).use(httpErrorHandler()).use(validateTokenParam);
