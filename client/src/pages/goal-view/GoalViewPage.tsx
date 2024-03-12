d0ce90f4c53a4989708d0accba2cf3d7d05b7a20
tweppy <tweppy@gmail.com>
2024-03-12 11:23:40 +0100

    Add GoalViewPage

--- client/src/pages/goal-view/GoalViewPage.tsx
+++ client/src/pages/goal-view/GoalViewPage.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { GoalCard } from "../../components/GoalCard/GoalCard";
+import { Goal } from "../../interfaces";
+import { getGoalData } from "../../utils/helpers";
 
 export const GoalViewPage = () => {
+  const [goal, setGoal] = useState<Goal>();
+  
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await getGoalData(id as string)
+      setGoal(result);
+    }
+    fetchData();
+  }, [id]);
+
   return (
-    <div>GoalViewPage</div>
-  )
-}
+    <main className="goal-page">
+      <header className="goal-page__header">
+        <h1 className="goal-page__title">Goal</h1>
+      </header>
+      <section>{goal && <GoalCard key={goal.goalId} {...goal} />}</section>
+    </main>
+  );
+};
