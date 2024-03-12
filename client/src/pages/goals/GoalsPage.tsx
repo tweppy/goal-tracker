7e821236608bac951cd5b7c77b1598f18eaf8cc7
tweppy <tweppy@gmail.com>
2024-03-12 11:24:02 +0100

    Refactor goalspage

--- client/src/pages/goals/GoalsPage.tsx
+++ client/src/pages/goals/GoalsPage.tsx
@@ -1,24 +1,16 @@
 import { getApiData } from "../../services/api";
-import { ApiSubmission, Goal } from "../../interfaces";
+import { Goal } from "../../interfaces";
+import { GoalCard } from "../../components/GoalCard/GoalCard";
 
 import { useEffect, useState } from "react";
-
-import { GoalCard } from "../../components/GoalCard/GoalCard";
 
 export const GoalsPage = () => {
   const [goals, setGoals] = useState<Goal[]>([]);
 
   const getUserGoals = async () => {
-    const apiSubmissionData: ApiSubmission = {
-      method: "GET",
-      link: "/goals",
-    };
-
     try {
-      const response = await getApiData(apiSubmissionData);
-      const foundGoals = response.body.goals;
-
-      setGoals(foundGoals);
+      const response = await getApiData({ method: "GET", link: "/goals" });
+      setGoals(response.body.goals);
     } catch (error) {
       console.error("Error fetching goals:", error);
     }
@@ -34,17 +26,10 @@ export const GoalsPage = () => {
         <h1 className="goals-page__title">Goals</h1>
       </header>
       <section className="goals-page__goals">
-        {goals.map(goal => {
-          return (
-            <GoalCard
-              key={goal.goalId}
-              id={goal.goalId}
-              title={goal.goalName}
-              desc={goal.description}
-            />
-          );
-        })}
-
+        {goals.map(goal => (
+          <GoalCard key={goal.goalId} {...goal} />
+        ))}
+        
         {goals.length === 0 && <p className="empty">No goals found</p>}
       </section>
     </main>
