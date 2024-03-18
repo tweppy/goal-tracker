235522538159912cf8ba3248f6ecea909006c563
tweppy <tweppy@gmail.com>
2024-03-18 16:16:39 +0100

    Add remove goal function

--- client/src/pages/goal-view/GoalViewPage.tsx
+++ client/src/pages/goal-view/GoalViewPage.tsx
@@ -1,17 +1,20 @@
 import "./style.scss";
 
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import { GoalCard } from "../../components/GoalCard/GoalCard";
 import { Goal } from "../../interfaces";
 import { getGoalData } from "../../utils/helpers";
 import { Layout } from "../../components/Layout/Layout";
+import { submitToApi } from "../../services/api";
 
 export const GoalViewPage = () => {
   const [goal, setGoal] = useState<Goal>();
 
   const { id } = useParams();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchData() {
@@ -21,6 +24,20 @@ export const GoalViewPage = () => {
     fetchData();
   }, [id]);
 
+  const handleDelete = async () => {
+    const result = await submitToApi({ method: "DELETE", link: `/goals/${id}` });
+
+    if (result.success === true) {
+      console.log("goal removed");
+      setTimeout(() => {
+        navigate("/goals");
+        // add some notif that goal was marked as completed
+      }, 2000);
+    } else {
+      console.log("Failed to remove goal");
+    }
+  };
+
   return (
     <Layout>
       <main className="goal-page">
@@ -28,6 +45,9 @@ export const GoalViewPage = () => {
           <h1 className="goal-page__title">Goal</h1>
         </header>
         <section>{goal && <GoalCard key={goal.goalId} {...goal} />}</section>
+        <section>
+          <button onClick={handleDelete}>delete goal</button>
+        </section>
       </main>
     </Layout>
   );
