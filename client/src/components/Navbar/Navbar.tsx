be4313bb6faf5f75129ea7aaf88de46e13b1692c
tweppy <tweppy@gmail.com>
2024-03-18 11:42:58 +0100

    Add navbar
    - svg navicons
    - basic temporary style

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import "./style.scss";
+
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import navIconOpen from "../../assets/navicon-open.svg";
+import navIconClose from "../../assets/navicon-close.svg";
+
+const links = [
+  { title: "Home", url: "/" },
+  { title: "Goals", url: "/goals" },
+  { title: "Today", url: "/today" },
+  { title: "Progress", url: "/progress" },
+];
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>();
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <aside className="navicon" onClick={toggleMenu}>
+        <img src={isOpen ? navIconClose : navIconOpen} alt="nav icon" />
+      </aside>
+      <ul className={`navbar-links ${isOpen ? "active" : ""}`}>
+        {links.map((link, index) => (
+          <li key={index}>
+            {
+              <Link to={link.url} className="navbar-links__link">
+                {link.title}
+              </Link>
+            }
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
