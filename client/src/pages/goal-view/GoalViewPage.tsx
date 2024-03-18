a85a7f919c797f090a462f7498d7bdd73d28f190
tweppy <tweppy@gmail.com>
2024-03-18 21:34:07 +0100

    Add edit form

--- client/src/pages/goal-view/GoalViewPage.tsx
+++ client/src/pages/goal-view/GoalViewPage.tsx
@@ -7,10 +7,12 @@ import { GoalCard } from "../../components/GoalCard/GoalCard";
 import { Goal } from "../../interfaces";
 import { getGoalData } from "../../utils/helpers";
 import { Layout } from "../../components/Layout/Layout";
-import { submitToApi } from "../../services/api";
+import { submitBodyToApi, submitToApi } from "../../services/api";
+import { GoalForm } from "../../components/GoalForm/GoalForm";
 
 export const GoalViewPage = () => {
   const [goal, setGoal] = useState<Goal>();
+  const [showEditForm, setShowEditForm] = useState<boolean>(false);
 
   const { id } = useParams();
 
@@ -38,15 +40,34 @@ export const GoalViewPage = () => {
     }
   };
 
+  // add some notif that goal was marked as completed
+  const handleEdit = async (goal: Goal) => {
+    await submitBodyToApi({ data: goal, method: "PUT", link: `/goals/${id}` });
+    console.log("Submitted edited goal:", goal);
+  };
+
   return (
     <Layout>
       <main className="goal-page">
         <header className="goal-page__header">
           <h1 className="goal-page__title">Goal</h1>
         </header>
-        <section>{goal && <GoalCard key={goal.goalId} {...goal} />}</section>
-        <section>
+        <section className="goal-page__goal">
+          {goal && (
+            <GoalCard
+              key={goal.goalId}
+              goalId={goal.goalId || ""}
+              userId={goal.userId || ""}
+              {...goal}
+            />
+          )}
           <button onClick={handleDelete}>delete goal</button>
+        </section>
+
+        <section>
+          <button onClick={() => setShowEditForm(!showEditForm)}>edit goal</button>
+
+          {showEditForm && <GoalForm onSubmit={handleEdit} initialGoal={goal} />}
         </section>
       </main>
     </Layout>
