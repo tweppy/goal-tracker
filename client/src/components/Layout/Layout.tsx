d170ec0f1225662e28d845afbc86f6cd00b97943
tweppy <tweppy@gmail.com>
2024-03-18 15:47:18 +0100

    Update Layout.tsx

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -3,17 +3,19 @@ import "./style.scss";
 import { ReactNode } from "react";
 
 import { Navbar } from "../Navbar/Navbar";
+import { useAuth } from "../../auth/AuthContext";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export const Layout = ({ children }: LayoutProps) => {
-  const token = localStorage.getItem("token");
+  const { isAuthenticated } = useAuth();
+  console.log(isAuthenticated);
 
   return (
     <div className="layout">
-      {token && <Navbar />}
+      {isAuthenticated && <Navbar />}
       {children}
     </div>
   );
