f4df262a5b28840a7fffc8eb12b55c75aacaf208
tweppy <tweppy@gmail.com>
2024-03-19 13:57:53 +0100

    Add loading spinner

---
+++ client/src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,36 @@
+import "./style.scss";
+
+import { Layout } from "../Layout/Layout";
+
+export const LoadingSpinner = () => {
+  return (
+    <Layout>
+      <div className="loading-spinner">
+        <svg
+          width="80"
+          height="80"
+          viewBox="0 0 38 38"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke="#00BFFF"
+        >
+          <g fill="none" fillRule="evenodd">
+            <g transform="translate(1 1)" strokeWidth="2">
+              <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
+              <path d="M36 18c0-9.94-8.06-18-18-18">
+                <animateTransform
+                  attributeName="transform"
+                  type="rotate"
+                  from="0 18 18"
+                  to="360 18 18"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+              </path>
+            </g>
+          </g>
+        </svg>
+        <p>Loading...</p>
+      </div>
+    </Layout>
+  );
+};
