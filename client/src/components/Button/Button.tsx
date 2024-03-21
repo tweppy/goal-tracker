5c6dcc69346d3c67e86a634e8c91d1c646c922d5
tweppy <tweppy@gmail.com>
2024-03-21 23:55:29 +0100

    Add button design

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+/* eslint-disable react-refresh/only-export-components */
+import { ReactNode } from "react";
+import "./style.scss";
 
-export const Button = () => {
-  return (
-    <div>Button</div>
-  )
+export enum ButtonType {
+  "default" = "default",
+  "delete" = "delete"
 }
+
+export interface ButtonProps {
+  children: ReactNode;
+  disabled?: boolean;
+  onClick: () => void;
+  type: ButtonType;
+}
+
+export const Button = ({ children, disabled, onClick, type = ButtonType.default }: ButtonProps) => {
+  return (
+    <button
+      role="button"
+      className={`btn__${type}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
