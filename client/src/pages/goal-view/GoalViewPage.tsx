55ebc9da5e9f99f410ad7ef65d3c93de299191ce
tweppy <tweppy@gmail.com>
2024-03-25 09:59:58 +0100

    Update GoalViewPage.tsx

--- client/src/pages/goal-view/GoalViewPage.tsx
+++ client/src/pages/goal-view/GoalViewPage.tsx
@@ -3,19 +3,21 @@ import "./style.scss";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-import { GoalCard } from "../../components/GoalCard/GoalCard";
+import { GoalCard, GoalCardType } from "../../components/GoalCard/GoalCard";
 import { Goal } from "../../interfaces";
 import { getGoalData } from "../../utils/helpers";
-import { Layout } from "../../components/Layout/Layout";
+import { Layout, LayoutType } from "../../components/Layout/Layout";
 import { submitBodyToApi, submitToApi } from "../../services/api";
 import { GoalForm } from "../../components/GoalForm/GoalForm";
 import { notifyError, notifySuccess } from "../../utils/notifications";
 import { LoadingSpinner } from "../../components/LoadingSpinner/LoadingSpinner";
+import { Modal } from "../../components/Modal/Modal";
+import { Button, ButtonType } from "../../components/Button/Button";
 
 export const GoalViewPage = () => {
   const [goal, setGoal] = useState<Goal>();
-  const [showEditForm, setShowEditForm] = useState<boolean>(false);
   const [loading, setLoading] = useState(true);
+  const [showModal, setShowModal] = useState<boolean>(false);
 
   const { id } = useParams();
 
@@ -56,29 +58,47 @@ export const GoalViewPage = () => {
     }
   };
 
+  const handleOpenModal = async () => {
+    setShowModal(true);
+  };
+
+  const handleViewProgress = () => {
+    navigate("/progress/" + id);
+  };
+
   return goal && !loading ? (
-    <Layout>
+    <Layout type={!showModal ? LayoutType.fullScreenNav : LayoutType.fullScreen}>
       <main className="goal-page">
         <header className="goal-page__header">
-          <h1 className="goal-page__title">Goal</h1>
+          <h1 className="goal-page__title">{goal.goalName}</h1>
         </header>
         <section className="goal-page__goal">
           {goal && (
             <GoalCard
+              type={GoalCardType.goalView}
               key={goal.goalId}
               goalId={goal.goalId || ""}
-              userId={goal.userId || ""}
               {...goal}
             />
           )}
-          <button onClick={handleDelete}>Delete goal</button>
+          <section className="goal__buttons">
+            <Button type={ButtonType.default} onClick={handleOpenModal}>
+              Edit Goal
+            </Button>
+            <Button type={ButtonType.default} onClick={handleViewProgress}>
+              View Progress
+            </Button>
+            <Button type={ButtonType.delete} onClick={handleDelete}>
+              Delete Goal
+            </Button>
+          </section>
         </section>
 
-        <section>
-          <button onClick={() => setShowEditForm(!showEditForm)}>Edit goal</button>
-
-          {showEditForm && <GoalForm onSubmit={handleEdit} initialGoal={goal} />}
-        </section>
+        {showModal && (
+          <Modal onClose={() => setShowModal(false)}>
+            <GoalForm heading="Edit Goal" onSubmit={handleEdit} initialGoal={goal} />
+          </Modal>
+        )}
       </main>
     </Layout>
   ) : (
