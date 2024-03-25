fef30c2bb2dac852bc5acb9ecc6184fdad976e8f
tweppy <tweppy@gmail.com>
2024-03-25 10:07:30 +0100

    Update ProgressGoalViewPage.tsx

--- client/src/pages/progress-goal/ProgressGoalViewPage.tsx
+++ client/src/pages/progress-goal/ProgressGoalViewPage.tsx
@@ -5,9 +5,10 @@ import { useParams } from "react-router-dom";
 
 import { submitToApi } from "../../services/api";
 import { CompletedGoal } from "../../interfaces";
-import { ProgressCard } from "../../components/ProgressCard/ProgressCard";
-import { Layout } from "../../components/Layout/Layout";
+import { Layout, LayoutType } from "../../components/Layout/Layout";
 import { LoadingSpinner } from "../../components/LoadingSpinner/LoadingSpinner";
+import { ProgressCircle } from "../../components/ProgressCircle/ProgressCircle";
+import { calculateProgress, calculateProgressForPreviousWeek } from "../../utils/progressHelpers";
 
 export const ProgressGoalViewPage = () => {
   const [progressGoal, setProgressGoal] = useState<CompletedGoal[]>([]);
@@ -30,15 +31,33 @@ export const ProgressGoalViewPage = () => {
     getGoalProgress();
   }, []);
 
+  const repeatType = progressGoal[0]?.repeatType;
+  const progress = calculateProgress(progressGoal, repeatType);
+  const lastWeekProgress = calculateProgressForPreviousWeek(progressGoal, repeatType);
+  console.log(lastWeekProgress)
+
   return progressGoal && !loading ? (
-    <Layout>
+    <Layout
+      type={LayoutType.default}
+      title={progressGoal[0].goalName}
+      description="Any progress is a step closer to your goal.
+      Even if you miss days, keep going!"
+    >
       <main className="progress-goal-page">
         <section className="progress-goal-page__wrapper">
-          {progressGoal.length > 0 ? (
-            <ProgressCard {...progressGoal[0]} completedOn={progressGoal} showDetails={true} />
-          ) : (
-            <p className="empty">No progress found for this goal</p>
-          )}
+          <section className="progress__this-week">
+            <h2 className="this-week__title">This week</h2>
+            <ProgressCircle progress={progress} />
+            <p className="this-week__description">This week, you've completed {progress}% so far</p>
+          </section>
+
+          <section className="progress__last-week">
+            <h2 className="last-week__title">Last week</h2>
+            <ProgressCircle progress={lastWeekProgress} />
+            <p className="last-week__description">Last week, you completed the goal {lastWeekProgress}%</p>
+          </section>
+
+          {/* <p className="empty">No progress found for this goal</p> */}
         </section>
       </main>
     </Layout>
